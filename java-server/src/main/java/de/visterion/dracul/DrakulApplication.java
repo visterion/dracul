33702f67c0e6518392c03b8febf2705c98434a21
vivu <vivu@users.noreply.github.com>
2026-05-16 23:00:35 +0200

    feat(java-server): add Maven skeleton, Spring Boot 4 pom, application config

---
+++ java-server/src/main/java/de/visterion/dracul/DrakulApplication.java
@@ -0,0 +1,11 @@
+package de.visterion.dracul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrakulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DrakulApplication.class, args);
+    }
+}
