685d172f0b482941380872d534471b84cc1fe61e
vivu <vivu@users.noreply.github.com>
2026-07-17 17:10:42 +0200

    refactor(daywalker): drop unused MathContext import

--- java-server/src/main/java/de/visterion/dracul/daywalker/DaywalkerEventEngine.java
+++ java-server/src/main/java/de/visterion/dracul/daywalker/DaywalkerEventEngine.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
